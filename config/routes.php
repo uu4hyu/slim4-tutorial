c02a6eaaaa6985b3eee497b0fbbf9e08fe492707
wahyu nurrahman <wahyunurrahman@gmail.com>
2020-03-09 23:02:35 +0700

    first commit

--- config/routes.php
+++ config/routes.php
@@ -4,5 +4,6 @@ use Slim\App;
 
 return function (App $app) {
     $app->get('/', \App\Action\HomeAction::class);
+    $app->get('/users/{id}', \App\Action\UserReadAction::class);
     $app->post('/users', \App\Action\UserCreateAction::class);
 };
